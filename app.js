ae370d144dc8f2661cc4b5e18d528a49713ab793
MohsinAwan214 <nomanmalik5320@gmail.com>
2024-10-04 19:42:21 -0700

    Add initial file

---
+++ app.js
@@ -0,0 +1,31 @@
+let form = document.querySelector("#form");
+let main = document.querySelector("#main");
+main.style.display = "none"
+form.addEventListener("submit", async (app) => {
+    app.preventDefault();
+    let imgTag = main.children[0];
+    let name = main.children[1];
+    let following = main.children[2].children[0];
+    let followers = main.children[2].children[1];
+    let repo = main.children[2].children[2];
+    let link = main.children[3].children[0];
+    
+    let value = app.target.children[0].value;
+    
+    let userApi = `https://api.github.com/users/${value}`;
+    
+    try {
+      main.style.display = "block"
+    let response = await axios(userApi);
+    imgTag.src = response.data.avatar_url;
+    name.innerHTML += response.data.name;
+    link.href = response.data.html_url;
+    repo.innerHTML += response.data.public_repos;
+    following.innerHTML += response.data.following;
+    followers.innerHTML += response.data.followers;
+  } catch (err) {
+    console.log(err.response.data.message);
+  }
+});
+
+// 
